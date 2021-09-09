a17cc2ce7a64e7fd6ec071df1c3c07ba98d41e1b
Benjamin Tosello <benjamintosello@MacBook-Pro-de-Benjamin.local>
2021-09-09 19:44:02 -0300

    new upload

--- vue/src/store/generated/cheqd/cheqd-node/cheqdid.cheqdnode.cheqd/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Nym } from "./module/types/cheqd/nym";
-export { Nym };
-declare const _default;
-export default _default;
--- vue/src/store/generated/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: {
-    CheqdCheqdNodeCheqdidCheqdnodeCheqd: (store: any) => void;
-};
-export default _default;
